f4fc5355017d0d0b50f9b3c034c7f1547bbbce2b
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-06 19:24:23 +0300

    Пропсы

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import ButtonsCard from "./buttonsCard";
-const Profile = () => {
+const Profile = (props) => {
+    const card1 = {
+        kind: "Свинья",
+        tel: "++0(123)567-89-01",
+        status: "Активное",
+        name: "Черчилль",
+        date: "29.11.2023",
+        desc: "В Центральном районе Санкт-Петербурга обнаружена свинья с ошейником. Отзывается на кличку Черчилль"
+    }
+    const card2 = {
+        kind: "Курица",
+        tel: "+764642552",
+        status: "Найден",
+        name: "Сергей",
+        date: "01.12.2023",
+        desc: "В Приморском районе Санкт-Петербурга обнаружен вислоухий кролик"
+    }
     return (
         <main style={{ "minHeight": "70vh" }}>
             <h2 className="text-center text-white bg-primary m-3">Личный кабинет</h2>
@@ -8,32 +24,32 @@ const Profile = () => {
                 <div className="row mb-3">
                     <label htmlFor="name" className="col-form-label">Имя</label>
                     <div className="col-sm-10 w-100">
-                        <input className="form-control m-auto" id="name" value="Иван Иванович Иванов" readonly />
+                        <input className="form-control m-auto" id="name" value={props.data.name} readOnly />
                     </div>
                 </div>
                 <button className="btn btn-primary">Изменить</button>
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-form-label">Эл. почта</label>
                     <div className="col-sm-10 w-100">
-                        <input type="email" value="user@user.com" className="form-control m-auto" id="inputEmail3" readonly />
+                        <input type="email" value={props.data.email} className="form-control m-auto" id="inputEmail3" readOnly />
                     </div>
                 </div>
                 <button className="btn btn-primary">Изменить</button>
                 <div className="row mb-3">
                     <label htmlFor="tel" className="col-form-label">Телефон</label>
                     <div className="col-sm-10 w-100">
-                        <input type="tel" className="form-control m-auto" id="tel" value="89112345678" readonly />
+                        <input type="tel" className="form-control m-auto" id="tel" value={props.data.tel} readOnly />
                     </div>
                 </div>
                 <button className="btn btn-primary">Изменить</button>
-                <p className="col-form-label">Количество дней с момента регистрации: 5</p>
+                <p className="col-form-label">Количество дней с момента регистрации: {props.data.days}</p>
                 <button className="btn btn-primary">Выйти</button>
             </form>
             <h2 className="text-center text-white bg-primary m-3">Ваши карточки</h2>
             <div className="row justify-content-center">
-                <ButtonsCard/>
-                <ButtonsCard/>
-                <ButtonsCard/>
+                <ButtonsCard data={card1} />
+                <ButtonsCard data={card1}/>
+                <ButtonsCard data={card1}/>
             </div>
         </main>
     );
--- src/pages/profile_page.jsx
+++ src/pages/profile_page.jsx
@@ -1,15 +1,19 @@
 import Profile from "../components/profile";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import { useState } from "react";
 
 const ProfilePage = () => {
-    return (  
+    const user = { name: 'Иванов Иван Мванович', email: 'admin@yandex.ru', tel: '+7911-954-45-45', days: 45678 };
+    //const [user, setUser] = useState({});
+    // setUser(user => ({ ...user, ...pavel }));
+    return (        
         <div>
-            <Header/>
-            <Profile/>
-            <Footer/>
+            <Header />
+            <Profile data={user}/>
+            <Footer />
         </div>
     );
 }
- 
+
 export default ProfilePage;
