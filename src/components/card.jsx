ba86cfff44432b622a3cbb1aaaad746a94b64148
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-07 11:36:21 +0300

    Подготовка к запросам

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,12 +6,15 @@ const Card = (props) => {
         <div className="card" style={{ "width": "18rem" }}>
             <img src={pig_img} className="card-img-top" alt="pig" height="300" />
             <div className="card-body">
-                <h5 className="card-title text-center">{props.data.kind}</h5>
-                <p className="card-text">{props.data.desc}</p>
+                <h5 className="card-title text-center">{props.data.name}</h5>
+                <p className="card-text">{props.data.description}</p>
             </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">Имя: {props.data.name}</li>
-                <li className="list-group-item">Номер телефона нашедшего: {props.data.tel}</li>
+            <ul className="list-group list-group-flush" style={{"minHeight":"320px"}}>
+            <li className="list-group-item">ID: {props.data.id}</li>
+                <li className="list-group-item">Вид: {props.data.kind}</li>
+                <li className="list-group-item">Номер телефона нашедшего: {props.data.phone}</li>
+                <li className="list-group-item">Район: {props.data.district}</li>
+                <li className="list-group-item">Номер чипа: {props.data.mark}</li>
                 <li className="list-group-item">Дата: {props.data.date}</li>
             </ul>
         </div>
