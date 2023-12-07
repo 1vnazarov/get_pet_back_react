e7f54c7b8642bd64fea4de6f8ae4935e2069cc9a
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-07 19:04:25 +0300

    Авторизация

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+
+const request = () => {
+    fetch("https://pets.сделай.site/api/login", {method: "POST", body: new FormData(document.getElementById("login"))})
+            .then(response => response.json())
+            .then(result => {
+                localStorage.setItem("token", result.data.token);
+                console.log(result);
+            })
+            .catch(error => console.log('error', error));
+}
+
 const Auth = () => {
     return (
         <main style={{ "minHeight": "70vh" }}>
             <h2 className="text-center text-white bg-primary m-3">Войти</h2>
-            <form className="w-25 m-auto minContainer">
+            <form className="w-25 m-auto minContainer" id="login">
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-form-label">Эл. почта</label>
                     <div className="col-sm-10 w-100">
-                        <input type="email" className="form-control minWidth" id="inputEmail3" required />
+                        <input name="email" type="email" className="form-control minWidth" id="inputEmail3" required />
                     </div>
                 </div>
                 <div className="row mb-3">
                     <label htmlFor="inputPassword3" className="col-form-label">Пароль</label>
                     <div className="col-sm-10 w-100">
-                        <input type="password" className="form-control minWidth" id="inputPassword3" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{7,}$" required />
+                        <input name="password" type="password" className="form-control minWidth" id="inputPassword3" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{7,}$" required />
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Войти</button>
+                <p className="btn btn-primary" onClick={request}>Войти</p>
                 <p className="mb-3">Еще не зарегистрированы?</p>
                 <Link className="btn btn-primary" to={'/register'}>На страницу регистрации</Link>
             </form>
