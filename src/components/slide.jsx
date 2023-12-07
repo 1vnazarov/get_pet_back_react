5a2d0ac266d1355ec0fe257a5a4624df5abafbf6
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-07 13:10:34 +0300

    Слайдер с сервера

--- src/components/slide.jsx
+++ src/components/slide.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import dog_img from "../images/dog.jpg";
 
 const Slide = (props) => {
     return (
         <div className={"carousel-item" + (props.active ? " active" : "")} >
-            <img src={dog_img} className="rounded mx-auto d-block" alt="Собака" width="300" />
-            <h2 className="text-center">{props.data.title}</h2>
-            <h3 className="text-center">{props.data.desc}</h3>
+            <img src={'https://pets.сделай.site' + props.data.image} className="rounded mx-auto d-block" alt="Собака" width="300" />
+            <h2 className="text-center">{props.data.kind}</h2>
+            <h3 className="text-center">{props.data.description}</h3>
         </div>
     );
 }
+
 
 export default Slide;
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,45 +1,70 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Slide from "./slide";
 
-const Slider = () => {
-    const slide1 = {
-        title: "Найдена собака",
-        desc: "В Приморском районе найдена собака"
-    }
-    const slide2 = {
-        title: "Найдена кошка",
-        desc: "Около станции метро Площадь Восстания обнаружена белая кошка"
-    }
-    const slide3 = {
-        title: "Найден хомяк",
-        desc: "На улице Малая Конюшенная, 8 бегает хомяк!"
-    }
+const Loader = (props) => {
     return (
-        <div id="carouselExampleIndicators" className="carousel slide m-auto bg-success bg-opacity-25 w-75 p-2"
-            data-bs-ride="carousel" style={{ "minHeight": "400px" }}>
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+        <div className='justify-content-center align-items-center' id='loader' style={props.display}>
+            <div className='fs-1 text-success'>...Идет загрузка</div>
+        </div>
+    );
+}
+
+const Slider = () => {
+    const [slide, setSlide] = useState({ data: { pets: [] } });
+    const [show, setShow] = useState({ display: 'flex' });
+    useEffect(() => request(slide, setSlide), []);
+
+    const request = (slide, setSlide) => {
+        fetch("https://pets.сделай.site/api/pets/slider", { method: 'GET' })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                setSlide(result)
+                setShow({ display: 'none' });
+
+            })
+            .catch(error => console.log('error', error));
+
+    }
+
+    const slides = slide.data.pets.map((pet, index) => {
+        if (index == 0) {
+            return <Slide data={pet} key={index} active={1} />;
+        } else {
+            return <Slide data={pet} key={index}/>;
+
+        }
+    });
+
+    const indicators = slide.data.pets.map((pet, index) => {
+        if (index == 0) {
+            return <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" key={index + 'btn'}></button>;
+        } else {
+            return <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} aria-label={"Slide " + (Number(index) + 1)} key={index + 'btn'}></button>;
+
+        }
+    });
+
+    return (
+        <div>
+            <Loader display={show} />
+            <div id="carouselExampleIndicators" className="carousel slide m-auto bg-success bg-opacity-25 w-75 p-2"
+            data-bs-ride="carousel" style={{"minHeight": "400px"}}>
+                <div className="carousel-indicators">
+                    {indicators}
+                </div>
+                <div className="carousel-inner">
+                    {slides}
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-            <div className="carousel-inner">
-                <Slide data={slide1} active={1}/>
-                <Slide data={slide2}/>
-                <Slide data={slide3}/>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Предыдущий</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Следующий</span>
-            </button>
         </div>
     );
 }
