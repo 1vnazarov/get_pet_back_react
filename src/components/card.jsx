730a9be05395628407c689e8092c0951db209abd
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-07 16:09:28 +0300

    Правки карточки

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,12 +5,12 @@ const Card = (props) => {
         <div className="card m-3" style={{ "width": "18rem" }}>
             <img src={'https://pets.сделай.site' + props.data.photos} className="card-img-top" alt="pig" height="300" />
             <div className="card-body">
-                <h5 className="card-title text-center">{props.data.name}</h5>
+                <h5 className="card-title text-center">{props.data.kind}</h5>
                 <p className="card-text">{props.data.description}</p>
             </div>
             <ul className="list-group list-group-flush" style={{"minHeight":"320px"}}>
             <li className="list-group-item">ID: {props.data.id}</li>
-                <li className="list-group-item">Вид: {props.data.kind}</li>
+                <li className="list-group-item">Имя нашедшего: {props.data.name}</li>
                 <li className="list-group-item">Номер телефона нашедшего: {props.data.phone}</li>
                 <li className="list-group-item">Район: {props.data.district}</li>
                 <li className="list-group-item">Номер чипа: {props.data.mark}</li>
