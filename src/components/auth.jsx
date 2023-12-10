41594eaaf35d5865a3f5c24b78cb0a6363f1e4f6
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-12-10 20:59:01 +0300

    Валидация авторизации

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,62 +1,87 @@
 import { React, useState } from "react";
 import { Link } from 'react-router-dom';
 import CheckToken from "../modules/checkToken";
+import { validateEmail, validatePassword } from "../modules/validate";
 
 const Auth = () => {
-    const [user, setUser] = useState();
     const [token, setToken] = useState(CheckToken() ? localStorage.getItem("token") : null);
-    const request = (e) => {
-        e.preventDefault();
-        const form = document.getElementById('form')
-        if (!form.checkValidity()) {
-            e.stopPropagation()
-            form.classList.add('was-validated')
-            return
-        }
-        console.log(user)
-        fetch("https://pets.сделай.site/api/login", {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(user),
-            redirect: 'follow'
-        }).then(response => response.json()).then(result => {
-            console.log(result)
-            if ('data' in result) {
-                localStorage.setItem("token", result.data.token)
-                setToken(result.data.token);
-                document.getElementById('success').style.display = 'block'
-                document.getElementById('error').style.display = 'none'
-            } else {
-                document.getElementById('error').style.display = 'block'
-                document.getElementById('success').style.display = 'none'
-                document.getElementById('validationCustom02').value = ''
-            }
-        })
+    const request = () => {
+        fetch("https://pets.сделай.site/api/login", { method: 'POST', headers: { "Content-Type": "application/json" }, body: JSON.stringify(formData) })
+            .then(response => response.json()).then(result => {
+                console.log(result)
+                if ('data' in result) {
+                    localStorage.setItem("token", result.data.token)
+                    setToken(result.data.token);
+                    document.getElementById('success').style.display = 'block'
+                    document.getElementById('error').style.display = 'none'
+                } else {
+                    document.getElementById('error').style.display = 'block'
+                    document.getElementById('success').style.display = 'none'
+                    document.getElementById('validationCustom02').value = ''
+                }
+            })
     }
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [validations, setValidations] = useState({
+        email: false,
+        password: false,
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+
+        let error = "";
+        switch (name) {
+            case "email": error = validateEmail(value); break;
+            case "password": error = validatePassword(value); break;
+        }
+        setValidations((prev) => ({ ...prev, [name]: !error }));
+        setErrors((prevErrors) => ({ ...prevErrors, [name]: error }));
+    }
+
+    const handleSubmit = () => {
+        const emailError = validateEmail(formData.email);
+        const passwordError = validatePassword(formData.password);
+
+        if (emailError || passwordError) {
+            setErrors({ email: emailError, password: passwordError });
+        } else {
+            // Все данные валидны, выполняем запрос
+            request()
+        }
+    };
+
     return (
         <main style={{ "minHeight": "70vh" }}>
             <h2 className="text-center text-white bg-primary m-3">Войти</h2>
-            <form className="w-25 m-auto needs-validation minContainer" onSubmit={request} noValidate id='form'>
+            <form className="w-25 m-auto minContainer" id='form'>
                 <div className="row mb-3">
                     <label htmlFor="validationCustom01" className="col-form-label">Эл. почта</label>
                     <div className="col-sm-10 w-100">
-                        <input name="email" type="email" className="form-control minWidth" id="validationCustom01" required onChange={(e) => setUser({ ...user, email: e.target.value })} />
-                        <div className="invalid-feedback">
-                            Пожалуйста, введите адрес электронной почты
-                        </div>
+                        <input name="email" type="email" className={`form-control minWidth ${validations.email ? "is-valid" : ""}`} id="validationCustom01" required onChange={handleInputChange} />
+                        <span className="text-danger">{errors.email}</span>
                     </div>
                 </div>
                 <div className="row mb-3">
                     <label htmlFor="validationCustom02" className="col-form-label">Пароль</label>
                     <div className="col-sm-10 w-100">
-                        <input name="password" type="password" className="form-control minWidth" id="validationCustom02" required onChange={(e) => setUser({ ...user, password: e.target.value })} />
-                        <div className="invalid-feedback">
-                            Пожалуйста, введите пароль
-                        </div>
+                        <input name="password" type="password" className={`form-control minWidth ${validations.password ? "is-valid" : ""}`} id="validationCustom02" required onChange={handleInputChange} />
+                        <span className="text-danger">{errors.password}</span>
                     </div>
                 </div>
-                <input type="submit" value={"Войти"} className="btn btn-primary" />
-                <p className="mb-3">Еще не зарегистрированы?</p>
+                <p className="btn btn-primary" onClick={handleSubmit}>Войти</p>
+                <p className="mt-3">Еще не зарегистрированы?</p>
                 <Link className="btn btn-primary" to={'/register'}>На страницу регистрации</Link>
             </form>
             <p className='text-danger text-center' id='error' style={{ display: 'none' }}>Неправилный адрес электронной почты и e-mail</p>
