ce032281126708c3de62f7972c860b101e41e3df
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-12-10 20:25:19 +0300

    Занулить токен после регистрации

--- src/modules/registerRequest.jsx
+++ src/modules/registerRequest.jsx
@@ -2,6 +2,7 @@ const RegisterRequest = (body, callback = () => {}) => {
     fetch("https://pets.сделай.site/api/register", { method: "POST", body: body })
         .then(response => response = response.status).then(result => {
             console.log(result);
+            localStorage.setItem('token', null)
             callback(result)
         }).catch(error => console.log('error', error));
 }
