57a8d62dce6d68febd630b40dfcf6461635b1066
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-12-12 19:01:18 +0300

    Правки футера

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer = () => {
     return (
-        <footer className="border-bottom bg-primary mt-3">
+        <footer className="border-bottom bg-primary mt-3" style={{ bottom: 0}}>
             <p className="text-white p-3">GET PET BACK © Copyright, 2023</p>
             <p className="text-white p-3">Все права защищены</p>
         </footer>
