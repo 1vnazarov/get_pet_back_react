2213ebb7b0bc8ada3a1ec8566f618b6b3184bb1f
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-12-13 23:37:25 +0300

    Поиск

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,8 +6,17 @@ import { QuickSearchRequest } from "../modules/requests";
 const Search = () => {
     const location = useLocation();
     const query = location.state?.query
-
     const [card, setCard] = useState({ data: { orders: [] } });
+    const [kind, setKind] = useState('')
+    const [district, setDistrict] = useState('')
+
+    const searchRequest = (card, setCard) => {
+        fetch(`https://pets.сделай.site/api/search/order?district=${district}&kind=${kind}`).then(response => response.json())
+            .then(result => {
+                console.log(result)
+                setCard(result)
+            }).catch(error => console.log('error', error));
+    } 
 
     useEffect(() => {
         QuickSearchRequest(query, card, setCard)
@@ -20,16 +29,18 @@ const Search = () => {
                 <div className="row mb-3">
                     <div className="col-sm-10 w-100">
                         <label htmlFor="input3" className="col-form-label">Район</label>
-                        <input className="form-control m-auto" id="input3" required />
+                        <input className="form-control m-auto" id="input3" onChange={(e) => setDistrict(e.target.value)}/>
                     </div>
                 </div>
                 <div className="row mb-3">
                     <div className="col-sm-10 w-100">
                         <label htmlFor="inputPassword3" className="col-form-label">Вид</label>
-                        <input className="form-control w-100" id="inputPassword3" required />
+                        <input className="form-control w-100" onChange={(e) => setKind(e.target.value)} />
                     </div>
                 </div>
-                <p className="btn btn-primary">Найти</p>
+                <p className="btn btn-primary" onClick={() => {
+                    searchRequest(card, setCard)
+                }}>Найти</p>
             </form>
             <CardsList data={card.data.orders} itemsPerPage={6}/>
         </main>
